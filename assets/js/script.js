3c23114a62c92ec6f3f70ef626e75380f6822621
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-03-02 19:15:27 +0000

    Create arrays, mod plotly drawing function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,53 +4,53 @@ console.log("connected!")
 
   function calcDerivedParam() {
     // Get indata parameters for Panel 1
-    let bHalfP1 = parseInt(document.getElementById('bhalf-p1').innerText);
-    let cRootP1 = parseInt(document.getElementById('croot-p1').innerText);
-    let cTipP1 = parseInt(document.getElementById('ctip-p1').innerText);
-    let xDeltaP1 = parseInt(document.getElementById('xdelta-p1').innerText);
-    let deltaP1 = parseInt(document.getElementById('delta-p1').innerText);
+    var bHalfP1[1] = parseInt(document.getElementById('bhalf-p1').innerText);
+    var cRootP1[1] = parseInt(document.getElementById('croot-p1').innerText);
+    var cTipP1[1] = parseInt(document.getElementById('ctip-p1').innerText);
+    var xDeltaP1[1] = parseInt(document.getElementById('xdelta-p1').innerText);
+    var deltaP1[1] = parseInt(document.getElementById('delta-p1').innerText);
     // Calculate parameters for Panel 1
-    let lambdaP1 = cTipP1 / cRootP1;
-    let cSmcP1 = (cRootP1 + cTipP1) / 2;
-    let sHalfP1 = cRootP1 * cTipP1;
-    let arP1 = bHalfP1 / cSmcP1;
+    var lambdaP1[1] = cTipP1 / cRootP1;
+    var cSmcP1[1] = (cRootP1 + cTipP1) / 2;
+    var sHalfP1[1] = cRootP1 * cTipP1;
+    var arP1[1] = bHalfP1 / cSmcP1;
 
 
     // Get indata parameters for Panel 2
-    let bHalfP2 = parseInt(document.getElementById('bhalf-p2').innerText);
-    let cRootP2 = parseInt(document.getElementById('croot-p2').innerText);
-    let cTipP2 = parseInt(document.getElementById('ctip-p2').innerText);
-    let xDeltaP2 = parseInt(document.getElementById('xdelta-p2').innerText);
-    let deltaP2 = parseInt(document.getElementById('delta-p2').innerText);
+    var bHalfP2[2] = parseInt(document.getElementById('bhalf-p2').innerText);
+    var cRootP2[2] = parseInt(document.getElementById('croot-p2').innerText);
+    var cTipP2[2] = parseInt(document.getElementById('ctip-p2').innerText);
+    var xDeltaP2[2] = parseInt(document.getElementById('xdelta-p2').innerText);
+    var deltaP2[2] = parseInt(document.getElementById('delta-p2').innerText);
     // Calculate parameters for Panel 2
-    let lambdaP2 = cTipP2 / cRootP2;
-    let cSmcP2 = (cRootP2 + cTipP2) / 2;
-    let sHalfP2 = cRootP2 * cTipP2;
-    let arP2 = bHalfP2 / cSmcP2;
+    var lambdaP2[2] = cTipP2 / cRootP2;
+    var cSmcP2[2] = (cRootP2 + cTipP2) / 2;
+    var sHalfP2[2] = cRootP2 * cTipP2;
+    var arP2[2] = bHalfP2 / cSmcP2;
 
     // Get indata parameters for Panel 3
-    let bHalfP3 = parseInt(document.getElementById('bhalf-p3').innerText);
-    let cRootP3 = parseInt(document.getElementById('croot-p3').innerText);
-    let cTipP3 = parseInt(document.getElementById('ctip-p3').innerText);
-    let xDeltaP3 = parseInt(document.getElementById('xdelta-p3').innerText);
-    let deltaP3 = parseInt(document.getElementById('delta-p3').innerText);
+    var bHalfP3[3] = parseInt(document.getElementById('bhalf-p3').innerText);
+    var cRootP3[3] = parseInt(document.getElementById('croot-p3').innerText);
+    var cTipP3[3] = parseInt(document.getElementById('ctip-p3').innerText);
+    var xDeltaP3[3] = parseInt(document.getElementById('xdelta-p3').innerText);
+    var deltaP3[3] = parseInt(document.getElementById('delta-p3').innerText);
     // Calculate parameters for Panel 3
-    let lambdaP3 = cTipP3 / cRootP3;
-    let cSmcP3 = (cRootP3 + cTipP3) / 2;
-    let sHalfP3 = cRootP3 * cTipP3;
-    let arP3 = bHalfP3 / cSmcP3;
+    var lambdaP3[3] = cTipP3 / cRootP3;
+    var cSmcP3[3] = (cRootP3 + cTipP3) / 2;
+    var sHalfP3[3] = cRootP3 * cTipP3;
+    var arP3[3] = bHalfP3 / cSmcP3;
 
     // Calculate parameters for Complete wing (cw)
-    let bHalfCw = bHalfP1 + bHalfP2 + bHalfP3;
-    let cRootCw = cRootP1;
-    let cTipCw = cTipP3;
-    let xDeltaCw = "N/A";
-    let deltaCw = "N/A";
+    var bHalfCw[0] = bHalfP1 + bHalfP2 + bHalfP3;
+    var cRootCw[0] = cRootP1;
+    var cTipCw[0] = cTipP3;
+    var xDeltaCw[0] = "N/A";
+    var deltaCw[0] = "N/A";
     // Calculate parameters for Complete wing (cw)
-    let lambdaCw = cTipP3 / cRootP1;
-    let cSmcCw = "N/A";
-    let sHalfCw = sHalfP1 + sHalfP2 + sHalfP3; // Switched row between sHalfCw and arCw so that sHalfCw can be used to calculate arCw
-    let arCw = (bHalfCw)^2 / sHalfCw;
+    var lambdaCw[0] = cTipP3 / cRootP1;
+    var cSmcCw[0] = "N/A";
+    var sHalfCw[0] = sHalfP1 + sHalfP2 + sHalfP3; // Switched row between sHalfCw and arCw so that sHalfCw can be used to calculate arCw
+    var arCw[0] = (bHalfCw)^2 / sHalfCw;
   }
 
 // var lengthpara = document.getElementById("lenght");
@@ -62,78 +62,81 @@ console.log("connected!")
 
 function calcDrawCoord() {
   // Sweep line Panel 1
-  sweepRootP1X = cRootP1 * 0.01 * xDeltaP1
-  sweepRootP1Y = 0
-  sweepTipP1X = sweepRootP1X + bhalfP1 * sin(deltaP1) // Check if rad or deg is default in Java Script!
-  sweepTipP1Y = bhalfP1
+  const sweepP1X[0] = cRootP1 * 0.01 * xDeltaP1
+  const sweepP1Y[0] = 0
+  const sweepP1X[1] = sweepRootP1X + bhalfP1 * sin(deltaP1) // Check if rad or deg is default in Java Script!
+  const sweepP1Y[1] = bhalfP1
 
   // Sweep line Panel 2
-  sweepRootP2X = cRootP2 * 0.01 * xDeltaP2
-  sweepRootP2Y = bhalfP1
-  sweepTipP2X = sweepRootP2X + bhalfP2 * sin(deltaP2)
-  sweepTipP2Y = bhalfP2
+  const sweepP2X[0] = cRootP2 * 0.01 * xDeltaP2
+  const sweepP2Y[0] = bhalfP1
+  const sweepP2X[1] = sweepRootP2X + bhalfP2 * sin(deltaP2)
+  const sweepP2Y[1] = bhalfP2
 
   // Sweep line Panel 3
-  sweepRootP3X = cRootP3 * 0.01 * xDeltaP3
-  sweepRootP3Y = bhalfP2
-  sweepTipP3X = sweepRootP3X + bhalfP3 * sin(deltaP3)
-  sweepTipP3Y = bhalfP3
+  const sweepP3X[0] = cRootP3 * 0.01 * xDeltaP3
+  const sweepP3Y[0] = bhalfP2
+  const sweepP3X[1] = sweepRootP3X + bhalfP3 * sin(deltaP3)
+  const sweepP3Y[1] = bhalfP3
 
   // Panel 1
-  LeRootP1X = 0
-  LeRootP1Y = 0
+  const P1X[0] = 0
+  const P1Y[0] = 0
 
-  LeTipP1X = sweepTip1X - cTipP1 * 0.01 * xDeltaP1
-  LeTipP1Y = bhalfP1
+  const P1X[1] = sweepTip1X - cTipP1 * 0.01 * xDeltaP1
+  const P1Y[1] = bhalfP1
 
-  TeTipP1X = sweepTip1X - cTip * 0.01 * (100 - xDelta)
-  TeTipP1Y = bhalfP1
+  const P1X[2] = sweepTip1X - cTip * 0.01 * (100 - xDelta)
+  const P1Y[2] = bhalfP1
 
-  TeRootP1X = cRootCw
-  TeRootP1Y = 0
+  const P1X[3] = cRootCw
+  const P1Y[3] = 0
 
   // Panel 2
-  LeRootP2X = LeTipP1X
-  LeRootP2Y = LeTipP1Y
+  const P2X[0] = LeTipP1X
+  const P2Y[0] = LeTipP1Y
 
-  LeTipP2X = sweepTip2X - cTipP2 * 0.01 * xDeltaP2
-  LeTipP2Y = bhalfP2
+  const P2X[1] = sweepTip2X - cTipP2 * 0.01 * xDeltaP2
+  const P2Y[1] = bhalfP2
 
-  TeTipP2X = sweepTip2X - cTipP2 * 0.01 * (100 - xDeltaP2)
-  TeTipP2Y = bhalfP2
+  const P2X[2] = sweepTip2X - cTipP2 * 0.01 * (100 - xDeltaP2)
+  const P2Y[2] = bhalfP2
 
-  TeRootP2X = TeTipP1X
-  TeRootP2Y = TeTipP1Y
+  const P2X[3] = TeTipP1X
+  const P2Y[3] = TeTipP1Y
 
   // Panel 3
-  LeRootP3X = LeTipP2X
-  LeRootP3Y = LeTipP2Y
+  const P3X[0] = LeTipP2X
+  const P3Y[0] = LeTipP2Y
 
-  LeTipP3X = sweepTip3X - cTipP3 * 0.01 * xDeltaP3
-  LeTipP3Y = bhalfP3
+  var P3X[1] = sweepTip3X - cTipP3 * 0.01 * xDeltaP3
+  const P3Y[1] = bhalfP3
 
-  TeTipP3X = sweepTip3X - cTipP3 * 0.01 * (100 - xDeltaP3)
-  TeTipP3Y = bhalfP3
+  const P3X[2] = sweepTip3X - cTipP3 * 0.01 * (100 - xDeltaP3)
+  const P3Y[2] = bhalfP3
 
-  TeRootP3X = TeTipP2X
-  TeRootP3Y = TeTipP2Y
+  const P3X[3] = TeTipP2X
+  const P3Y[3] = TeTipP2Y
 }
 
-
+// Define size of graph axis for the drawing. the graph starts at origo so no need to calculate min values for x and y
+const xAxisMaxP[0] = max(P1X);
+const xAxisMaxP[1] = max(P2X);
+const xAxisMaxP[2] = max(P3X);
+const xAxisMax = max(xAxisMaxP);
+const yAxisMax = bhalfP3;
 
 // Got this from stackoverflow but it does not work when showing it: import plotly.graph_objects as go
 
 
-const xPanel1 = [0, 1, 5, 7];
-const yPanel1 = [0, 25, 25, 0];
+// const P1X = [0, 1, 5, 7];
+// const P1Y = [0, 25, 25, 0];
 
-//const xPanel1 = [1, 2, 3, 5];
-//const yPanel1 = [25, 35, 35, 25,];
 
 // Define Data
 const data = [{
-  x: xPanel1,
-  y: yPanel1,
+  x: P1X,
+  y: P1Y,
   // x: xPanel2,
   // y: yPanel2,
   mode: "lines"
@@ -142,8 +145,8 @@ const data = [{
 
 // Define Layout
 const layout = {
-  // xaxis: {range: [0, 20], title: "longitudinal"},
-  // yaxis: {range: [0, 40], title: "lateral"},
+  xaxis: {range: [0, xAxisMax]} // , title: "longitudinal"},
+  yaxis: {range: [0, yAxisMax]} // , title: "lateral"},
   // title: "WING with x nr. of panels"
 };
 
