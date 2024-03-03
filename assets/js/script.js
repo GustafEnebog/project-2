0571f3738b32c97c96768ba6606055ed7186e0dc
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-03-03 11:27:32 +0000

    Add draft ver of calcBHalfCw +  event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,86 @@
 console.log("connected!")
 
+var bHalf
+var cRoot
+var cTip
+var xDelta
+var delta
+var lambda
+var cSmc
+var sHalf
+var ar
 
 
-  function calcDerivedParam() {
-    // Get indata parameters for Panel 1
-    var bHalf[1] = parseInt(document.getElementById('bhalf-p1').innerText);
-    var cRoot[1] = parseInt(document.getElementById('croot-p1').innerText);
-    var cTip[1] = parseInt(document.getElementById('ctip-p1').innerText);
-    var xDelta[1] = parseInt(document.getElementById('xdelta-p1').innerText);
-    var delta[1] = parseInt(document.getElementById('delta-p1').innerText);
-    // Calculate parameters for Panel 1
-    var lambda[1] = cTipP1 / cRootP1;
-    var cSmc[1] = (cRootP1 + cTipP1) / 2;
-    var sHalf[1] = cRootP1 * cTipP1;
-    var ar[1] = bHalfP1 / cSmcP1;
 
-
-    // Get indata parameters for Panel 2
-    var bHalf[2] = parseInt(document.getElementById('bhalf-p2').innerText);
-    var cRoot[2] = parseInt(document.getElementById('croot-p2').innerText);
-    var cTip[2] = parseInt(document.getElementById('ctip-p2').innerText);
-    var xDelta[2] = parseInt(document.getElementById('xdelta-p2').innerText);
-    var delta[2] = parseInt(document.getElementById('delta-p2').innerText);
-    // Calculate parameters for Panel 2
-    var lambda[2] = cTipP2 / cRootP2;
-    var cSmc[2] = (cRootP2 + cTipP2) / 2;
-    var sHalf[2] = cRootP2 * cTipP2;
-    var ar[2] = bHalfP2 / cSmcP2;
-
-    // Get indata parameters for Panel 3
-    var bHalf[3] = parseInt(document.getElementById('bhalf-p3').innerText);
-    var cRoot[3] = parseInt(document.getElementById('croot-p3').innerText);
-    var cTip[3] = parseInt(document.getElementById('ctip-p3').innerText);
-    var xDelta[3] = parseInt(document.getElementById('xdelta-p3').innerText);
-    var delta[3] = parseInt(document.getElementById('delta-p3').innerText);
-    // Calculate parameters for Panel 3
-    var lambda[3] = cTipP3 / cRootP3;
-    var cSmc[3] = (cRootP3 + cTipP3) / 2;
-    var sHalf[3] = cRootP3 * cTipP3;
-    var ar[3] = bHalfP3 / cSmcP3;
-
-    // Calculate parameters for Complete wing (cw)
+// Testing for how tot calculate bHalf for complete Wing
+function calcBHalfCw() {
+  var bHalf[1] = parseInt(document.getElementById('bhalf-p1').innerText);
+  var bHalf[2] = parseInt(document.getElementById('bhalf-p2').innerText);
+  var bHalf[3] = parseInt(document.getElementById('bhalf-p3').innerText);
+  if (bHalf[1]  != "" && bHalf[2]  != "" && bHalf[3]  != "" &&) {
     var bHalf[0] = bHalfP1 + bHalfP2 + bHalfP3;
-    var cRoot[0] = cRootP1;
-    var cTip[0] = cTipP3;
-    var xDelta[0] = "N/A";
-    var delta[0] = "N/A";
-    // Calculate parameters for Complete wing (cw)
-    var lambda[0] = cTipP3 / cRootP1;
-    var cSmc[0] = "N/A";
-    var sHalf[0] = sHalfP1 + sHalfP2 + sHalfP3; // Switched row between sHalfCw and arCw so that sHalfCw can be used to calculate arCw
-    var ar[0] = (bHalfCw)^2 / sHalfCw;
   }
+}
+
+bHalf[1].addEventListener("oninput", calcBHalfCw);
+bHalf[1].addEventListener("oninput", calcBHalfCw);
+bHalf[1].addEventListener("oninput", calcBHalfCw);
+// addEventListener("click", displayDate);
+// element.addEventListener("oninput", calcDerivedParam);
+
+
+
+
+function calcDerivedParam() {
+  // Get indata parameters for Panel 1
+  var bHalf[1] = parseInt(document.getElementById('bhalf-p1').innerText);
+  var cRoot[1] = parseInt(document.getElementById('croot-p1').innerText);
+  var cTip[1] = parseInt(document.getElementById('ctip-p1').innerText);
+  var xDelta[1] = parseInt(document.getElementById('xdelta-p1').innerText);
+  var delta[1] = parseInt(document.getElementById('delta-p1').innerText);
+  // Calculate parameters for Panel 1
+  var lambda[1] = cTipP1 / cRootP1;
+  var cSmc[1] = (cRootP1 + cTipP1) / 2;
+  var sHalf[1] = cRootP1 * cTipP1;
+  var ar[1] = bHalfP1 / cSmcP1;
+
+
+  // Get indata parameters for Panel 2
+  var bHalf[2] = parseInt(document.getElementById('bhalf-p2').innerText);
+  var cRoot[2] = parseInt(document.getElementById('croot-p2').innerText);
+  var cTip[2] = parseInt(document.getElementById('ctip-p2').innerText);
+  var xDelta[2] = parseInt(document.getElementById('xdelta-p2').innerText);
+  var delta[2] = parseInt(document.getElementById('delta-p2').innerText);
+  // Calculate parameters for Panel 2
+  var lambda[2] = cTipP2 / cRootP2;
+  var cSmc[2] = (cRootP2 + cTipP2) / 2;
+  var sHalf[2] = cRootP2 * cTipP2;
+  var ar[2] = bHalfP2 / cSmcP2;
+
+  // Get indata parameters for Panel 3
+  var bHalf[3] = parseInt(document.getElementById('bhalf-p3').innerText);
+  var cRoot[3] = parseInt(document.getElementById('croot-p3').innerText);
+  var cTip[3] = parseInt(document.getElementById('ctip-p3').innerText);
+  var xDelta[3] = parseInt(document.getElementById('xdelta-p3').innerText);
+  var delta[3] = parseInt(document.getElementById('delta-p3').innerText);
+  // Calculate parameters for Panel 3
+  var lambda[3] = cTipP3 / cRootP3;
+  var cSmc[3] = (cRootP3 + cTipP3) / 2;
+  var sHalf[3] = cRootP3 * cTipP3;
+  var ar[3] = bHalfP3 / cSmcP3;
+
+  // Calculate parameters for Complete wing (cw)
+  var bHalf[0] = bHalfP1 + bHalfP2 + bHalfP3;
+  var cRoot[0] = cRootP1;
+  var cTip[0] = cTipP3;
+  var xDelta[0] = "N/A";
+  var delta[0] = "N/A";
+  // Calculate parameters for Complete wing (cw)
+  var lambda[0] = cTipP3 / cRootP1;
+  var cSmc[0] = "N/A";
+  var sHalf[0] = sHalfP1 + sHalfP2 + sHalfP3; // Switched row between sHalfCw and arCw so that sHalfCw can be used to calculate arCw
+  var ar[0] = (bHalfCw) ^ 2 / sHalfCw;
+}
 
 // var lengthpara = document.getElementById("lenght");
 
@@ -131,7 +160,8 @@ const yAxisMax = bhalfP3;
 
 // const P1X = [0, 1, 5, 7];
 // const P1Y = [0, 25, 25, 0];
-
+const P1X = [0, 1, 5, 7];
+const P1Y = [0, 25, 25, 0];
 
 // Define Data
 const data = [{
@@ -145,8 +175,12 @@ const data = [{
 
 // Define Layout
 const layout = {
-  xaxis: {range: [0, xAxisMax]} // , title: "longitudinal"},
-  yaxis: {range: [0, yAxisMax]} // , title: "lateral"},
+  xaxis: {
+    range: [0, 8]
+  } // , title: "longitudinal"},                                                   xAxisMax
+  yaxis: {
+    range: [0, 26]
+  } // , title: "lateral"},                                                        yAxisMax
   // title: "WING with x nr. of panels"
 };
 
