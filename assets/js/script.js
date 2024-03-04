03edbffcba4059998b637b205fb0b25d200f7e80
Gustaf Enebog <gustaf.enebog@alumni.usc.edu>
2024-03-04 20:27:49 +0000

    debugg mispellings

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,21 @@ let delta = [null, null, null, null];
 
 // Get the inputs from the html-form
 function calcBHalf() {
-  bHalf[1] = parseInt(document.getElementById('bhalf-p1').innerText);
-  bHalf[2] = parseInt(document.getElementById('bhalf-p2').innerText);
-  bHalf[3] = parseInt(document.getElementById('bhalf-p3').innerText);
+  bHalf[1] = parseInt(document.getElementById('bhalf-p1').value);
+  bHalf[2] = parseInt(document.getElementById('bhalf-p2').value);
+  bHalf[3] = parseInt(document.getElementById('bhalf-p3').value);
+  //debugger
+  // console.log(bHalf[1] );
+  // console.log(bHalf[2] );
+  // console.log(bHalf[3] );
   if (bHalf[1] != "" && bHalf[2] != "" && bHalf[3] != "") {
     bHalf[0] = bHalf[1] + bHalf[2] + bHalf[3];
+    // console.log(bHalf[0] );
   }
 }
 
 function calcCRoot() {
-  cRoot[1] = parseInt(document.getElementById('croot-p1').innerText);
+  cRoot[1] = parseInt(document.getElementById('croot-p1').value);
   // cRoot[2] = parseInt(document.getElementById('croot-p2').innerText);
   // cRoot[3] = parseInt(document.getElementById('croot-p3').innerText);
   if (cRoot[1] != "") {
@@ -26,25 +31,25 @@ function calcCRoot() {
 }
 
 function calcCTip() {
-  cTip[1] = parseInt(document.getElementById('cTip-p1').innerText);
-  cTip[2] = parseInt(document.getElementById('cTip-p2').innerText);
-  cTip[3] = parseInt(document.getElementById('cTip-p3').innerText);
+  cTip[1] = parseInt(document.getElementById('ctip-p1').value);
+  cTip[2] = parseInt(document.getElementById('ctip-p2').value);
+  cTip[3] = parseInt(document.getElementById('ctip-p3').value);
   if (cTip[3] != "") {
     cTip[0] = cTip[3];
   }
 }
 
 function calcXDelta() {
-  xDelta[1] = parseInt(document.getElementById('xdelta-p1').innerText);
-  xDelta[2] = parseInt(document.getElementById('xdelta-p2').innerText);
-  xDelta[3] = parseInt(document.getElementById('xdelta-p3').innerText);
+  xDelta[1] = parseInt(document.getElementById('xdelta-p1').value);
+  xDelta[2] = parseInt(document.getElementById('xdelta-p2').value);
+  xDelta[3] = parseInt(document.getElementById('xdelta-p3').value);
   xDelta[0] = "n/a";
 }
 
 function calcDelta() {
-  delta[1] = parseInt(document.getElementById('delta-p1').innerText);
-  delta[2] = parseInt(document.getElementById('cTip-p2').innerText);
-  delta[3] = parseInt(document.getElementById('cTip-p3').innerText);
+  delta[1] = parseInt(document.getElementById('delta-p1').value);
+  delta[2] = parseInt(document.getElementById('delta-p2').value);
+  delta[3] = parseInt(document.getElementById('delta-p3').value);
   delta[0] = "n/a";
 }
 
@@ -165,34 +170,34 @@ let P3Y = [null, null, null, null];
 // function calcDrawCoord() {
   // Sweep line Panel 1
   function calcSweepP1() {
-  if (cRoot[1] != "" && xDelta[1] != "" && bhalf[1] != "" && bhalf[2] != "" && delta[1] != "") {
+  if (cRoot[1] != "" && xDelta[1] != "" && bHalf[1] != "" && bHalf[2] != "" && delta[1] != "") {
     sweepP1X[0] = cRoot[1] * 0.01 * xDelta[1];
     sweepP1Y[0] = 0;
     deltaP1Rad = (Math.PI / 180) * delta[1];
-    sweepP1X[1] = cRoot[1] * 0.01 * xDelta[1] + bhalf[1] * sin(deltaP1Rad); // Check if rad or deg is default in Java Script!
-    sweepP1Y[1] = bhalf[1];
+    sweepP1X[1] = cRoot[1] * 0.01 * xDelta[1] + bHalf[1] * Math.sin(deltaP1Rad); // Check if rad or deg is default in Java Script!
+    sweepP1Y[1] = bHalf[1];
   }
   }
 
   // Sweep line Panel 2
   function calcSweepP2() {
-  if (cRoot[2] != "" && xDelta[2] != "" && bhalf[2] != "" && bhalf[2] != "" && delta[2] != "") {
+  if (cRoot[2] != "" && xDelta[2] != "" && bHalf[2] != "" && bHalf[2] != "" && delta[2] != "") {
     sweepP2X[0] = cRoot[2] * 0.01 * xDelta[2];
-    sweepP2Y[0] = bhalf[1];
+    sweepP2Y[0] = bHalf[1];
     deltaP2Rad = (Math.PI / 180) * delta[2];
-    sweepP2X[1] = cRoot[2] * 0.01 * xDelta[2] + bhalf[1] * sin(deltaP2Rad); // Check if rad or deg is default in Java Script!
-    sweepP2Y[1] = bhalf[1] + bhalf[2];
+    sweepP2X[1] = cRoot[2] * 0.01 * xDelta[2] + bHalf[1] * Math.sin(deltaP2Rad); // Check if rad or deg is default in Java Script!
+    sweepP2Y[1] = bHalf[1] + bHalf[2];
   }
   }
 
   // Sweep line Panel 3
   function calcSweepP3() {
-  if (cRoot[3] != "" && xDelta[3] != "" && bhalf[3] != "" && bhalf[3] != "" && delta[3] != "") {
+  if (cRoot[3] != "" && xDelta[3] != "" && bHalf[3] != "" && bHalf[3] != "" && delta[3] != "") {
     sweepP3X[0] = cRoot[3] * 0.01 * xDelta[3];
-    sweepP3Y[0] = bhalf[1] + bhalf[2];
+    sweepP3Y[0] = bHalf[1] + bHalf[2];
     deltaP3Rad = (Math.PI / 180) * delta[3];
-    sweepP3X[1] = cRoot[3] * 0.01 * xDelta[3] + bhalf[3] * sin(deltaP3Rad); // Check if rad or deg is default in Java Script!
-    sweepP3Y[1] = bhalf[1] + bhalf[2] + bhalf[3];
+    sweepP3X[1] = cRoot[3] * 0.01 * xDelta[3] + bHalf[3] * Math.sin(deltaP3Rad); // Check if rad or deg is default in Java Script!
+    sweepP3Y[1] = bHalf[1] + bHalf[2] + bHalf[3];
   }
   }
 
@@ -202,10 +207,10 @@ let P3Y = [null, null, null, null];
   P1Y[0] = 0;
 
   P1X[1] = sweepP1X[1] - cTip[1] * 0.01 * xDelta[1];
-  P1Y[1] = bhalf[1];
+  P1Y[1] = bHalf[1];
 
   P1X[2] = sweepP1X[1] - cTip * 0.01 * (100 - xDelta[1]);
-  P1Y[2] = bhalf[1];
+  P1Y[2] = bHalf[1];
 
   P1X[3] = cRoot[1];
   P1Y[3] = 0;
@@ -214,35 +219,36 @@ let P3Y = [null, null, null, null];
   // Panel 2
   function calcP2() {
   P2X[0] = P1X[1];
-  P2Y[0] = bhalf[1];
+  P2Y[0] = bHalf[1];
 
-  P2X[1] = sweepP1X[2] - cTip[2] * 0.01 * xDelta[2];
-  P2Y[1] = bhalf[1] + bhalf[2];
+  P2X[1] = sweepP2X[1] - cTip[2] * 0.01 * xDelta[2];
+  P2Y[1] = bHalf[1] + bHalf[2];
 
-  P2X[2] = sweepP1X[2] - cTip * 0.01 * (100 - xDelta[2]);
-  P2Y[2] = bhalf[1] + bhalf[2];
+  P2X[2] = sweepP2X[1] - cTip * 0.01 * (100 - xDelta[2]);
+  P2Y[2] = bHalf[1] + bHalf[2];
 
   P2X[3] = P1X[2];
-  P2Y[3] = bhalf[1];
+  P2Y[3] = bHalf[1];
   }
 
   // Panel 3
   function calcP3() {
   P3X[0] = P2X[1];
-  P3Y[0] = bhalf[1] + bhalf[2];
+  P3Y[0] = bHalf[1] + bHalf[2];
 
-  P3X[1] = sweepTip[3] - cTip[3] * 0.01 * xDelta[3];
-  P3Y[1] = bhalf[1] + bhalf[2] + bhalf[3];
+  P3X[1] = sweepP3X[1] - cTip[3] * 0.01 * xDelta[3];
+  P3Y[1] = bHalf[1] + bHalf[2] + bHalf[3];
 
-  P3X[2] = sweepTip[3] - cTip[3] * 0.01 * (100 - xDelta[3]);
-  P3Y[2] = bhalf[1] + bhalf[2] + bhalf[3];
+  P3X[2] = sweepP3X[1] - cTip[3] * 0.01 * (100 - xDelta[3]);
+  P3Y[2] = bHalf[1] + bHalf[2] + bHalf[3];
 
   P3X[3] = P2X[2];
-  P3Y[3] = bhalf[1] + bhalf[2];
+  P3Y[3] = bHalf[1] + bHalf[2];
 }
 
 //-------------------- LET IT ALL HAPPEN (EVENTLISTENER)! --------------------
 function funcForEvent() {
+  console.log("funcForEvent");
   calcBHalf()
   calcCRoot()
   calcCTip()
@@ -260,4 +266,11 @@ function funcForEvent() {
   calcP3()
 }
 
+// Alternative 2 - Is it correct!? VariableInput.addEventListener('change', funcForEvent);
 document.body.addEventListener('oninput', funcForEvent); // do I need an element first on this row. Should I use "changes" instead of "oninput"
+
+// Alternative 2 - Is it correct!?
+let variableInputs = document.querySelectorAll(".input-output-area");
+for (let i = 0; i < variableInputs.length; i++) {
+  variableInputs[i].addEventListener('change', funcForEvent);
+}
